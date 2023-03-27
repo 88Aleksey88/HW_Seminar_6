04b82fc4e131f3cdf9b0fe02cf0ff88bb225d108
Aleksey <Sad_libra1988@mail.ru>
2023-03-27 21:43:20 +0700

    Done

---
+++ Задача№41/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputArray (int[] array)
+{
+    for (int i = 0 ; i < array.Length ; i++)
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+void PosNumbers (int [] array)
+{
+    int sum = 0;
+    for (int i = 0 ; i < array.Length ; i++)
+        if (array[i] > 0) sum += 1;
+    Console.WriteLine ($"Количество положительных чисел: {sum}");
+}
+Console.Clear();
+Console.Write("Количество вводимых чисел - ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[m];
+InputArray(array);
+PosNumbers (array);
+
+
